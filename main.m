bfc04f4ce7a40189495b7fb2b39bfff886e4d65a
Denis Yelkin <den.alekseevich@gmail.com>
2014-06-03 04:14:04 +0400

    Final report version

--- main.m
+++ main.m
@@ -5,11 +5,6 @@ if length(I(1,1,:)) > 1
     I=temp_I;
 end
 disp('now img monochrome');
-% I = addLight(I);
-% disp('add light');
-% imshow(I);
-% disp('show src img');
-% blur_I = blur(I, 0.1, 0.1, 0.2, 0.13, 0.13, 0.14, 0.3, 0.3);
 H = fspecial('gaussian', length(I(:, 1)), 2);
 blur_I = imfilter(I, H, 'replicate');
 disp('img blurred');
