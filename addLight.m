ea060d6e0903662378cd62d4f468b70c12e63e06
Denis Yelkin <den.alekseevich@gmail.com>
2014-06-04 21:38:03 +0400

    Report fixes

--- addLight.m
+++ addLight.m
@@ -1,10 +1,6 @@
 function [result] = addLight(img)
     min_val = min(min(img));
-    result = img - min_val;
-    delta = max(max(img));
-    for i = 1 : length(result(:, 1))
-        for j = 1 : length(result(1, :))
-            result(i, j) = round(double(img(i, j)) * 255 / delta);
-        end
-    end
+    result = double(img) - min_val;
+    delta = max(max(result));
+    result = uint8(result*255/delta);
 end
--- difference.m
+++ difference.m
@@ -1,3 +1,3 @@
 function [result] = difference(src_I, unblur_I)
-    result = addLight(uint8(round(abs(255 - double(src_I) + double(unblur_I)))));
+    result = addLight(abs(255 - double(src_I) + double(unblur_I)));
 end
