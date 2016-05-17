0e0f4f7edf6420aa924f35fcbc88efb6c9942564
Denis Elkin <den.alekseevich@gmail.com>
2016-05-18 03:00:22 +0400

    Before finish

--- main.m
+++ main.m
@@ -1,23 +1,24 @@
 clear;
+dispersion = 5;
 I = imread('lena.jpg');
 if length(I(1,1,:)) > 1
     temp_I = I(:,:,1);
     I=temp_I;
 end
-% figure;
-% imshow(I);
+figure;
+imshow(I);
 disp('now img monochrome');
-H = fspecial('gaussian', length(I(:, 1)), 5);
+H = fspecial('gaussian', length(I(:, 1)), dispersion);
 blur_I = imfilter(I, H, 'replicate');
 disp('img blurred');
-% figure;
-% imshow(blur_I);
+figure;
+imshow(blur_I);
 a = final_solver(I, blur_I, 500);
 disp('solver solved');
 unblur_I = final_unblur(blur_I, a);
 disp('img unblurred');
-% figure;
-% imshow(unblur_I)
+figure;
+imshow(unblur_I)
 
 shift = 4;
 sum = 0;
@@ -30,32 +31,4 @@ for i = shift : 1 : (length(I(:, 1)) - shift)
         count = count + 1;
     end;
 end;
-result = sqrt(1 / count * sum);
-
-% I = imread('cosmo1.jpg');
-% if length(I(1,1,:)) > 1
-%     temp_I = I(:,:,1);
-%     I=temp_I;
-% end
-% % figure;
-% % imshow(I);
-% H = fspecial('gaussian', length(I(:, 1)), 5);
-% blur_I = imfilter(I, H, 'replicate');
-% % figure;
-% % imshow(blur_I);
-% unblur_I = final_unblur(blur_I, a);
-% disp('img unblurred');
-% figure;
-% imshow(unblur_I)
-
-% % dif = difference(I, unblur_I);
-% % figure;
-% % imshow(dif);
-% 
-% m = 25;
-% lin = linear_solver(I, blur_I, m, 500);
-% disp('linear solver solved');
-% lin_unblur_I = unblur_lin(blur_I, lin);
-% disp('img unblurred by linear');
-% figure;
-% imshow(lin_unblur_I);
+deviation = sqrt(1 / count * sum);
